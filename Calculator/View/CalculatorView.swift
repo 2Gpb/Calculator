0ed6d85a60663576befd499bc4afefcb64549efb
2Gpb <Pvtyukhmenev@gmail.com>
2025-01-17 18:45:26 +0300

    add model

---
+++ Calculator/View/CalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//  Created by Peter on 17.01.2025.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
